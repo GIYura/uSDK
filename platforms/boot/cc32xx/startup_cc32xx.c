07a32befd560a6e9d8edd682daa9dfe24e3114ae
jura <jglotovyn@gmail.com>
2025-12-17 21:38:36 +0200

    UPDATE: locate vector table in RAM

--- platforms/boot/cc32xx/startup_cc32xx.c
+++ platforms/boot/cc32xx/startup_cc32xx.c
@@ -3,19 +3,25 @@
 extern uint32_t _estack;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
+extern uint32_t isr_vector;
+
+#define SCB_VTOR (*(volatile uint32_t*)0xE000ED08)
 
 int main(void);
 
 void Reset_Handler(void);
 
-__attribute__((section(".isr_vector"))) void (* const g_pfnVectors[])(void) = {
+__attribute__((section(".isr_vector")))
+void (* const g_pfnVectors[])(void) = {
     (void (*)(void))(&_estack),
     Reset_Handler,
 };
 
 void Reset_Handler(void)
 {
-    uint32_t *dst = &_sbss;
+    uint32_t* dst = &_sbss;
+
+    SCB_VTOR = (uint32_t)&isr_vector;
 
     while (dst < &_ebss)
     {
